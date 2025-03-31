880e1ff9a64790e0af6da8625ab8610bdae199a9
Dhiraj Chapagain <underhade@Dhirajs-MacBook-Air.local>
2025-03-31 11:29:36 +0545

    print name week1

---
+++ Assignment/Week1/name.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Your Name"; // Replace "Your Name" with the desired name
+    cout << "Hello, " << name << "!" << endl;
+    return 0;
+}
