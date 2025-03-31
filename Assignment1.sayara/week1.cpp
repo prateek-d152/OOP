4b2e357bb6b20cf39580837e0cfdee300190046f
sayaragautam <sayeragtm10@gmail.com>
2025-03-31 11:19:43 +0545

    Added week 1 test code

--- Assignment1.sayara/week1.cpp
+++ Assignment1.sayara/week1.cpp
@@ -1,1 +1,39 @@
-// hi im sayara
+#include <iostream>
+using namespace std;
+
+// Base class
+class Animal {
+public:
+    void makeSound() {
+        cout << "Animals make sounds!" << endl;
+    }
+};
+
+// Derived class
+class Dog : public Animal {
+public:
+    void makeSound() {
+        cout << "Dog barks: Woof woof!" << endl;
+    }
+};
+
+// Another Derived class
+class Cat : public Animal {
+public:
+    void makeSound() {
+        cout << "Cat meows: Meow meow!" << endl;
+    }
+};
+
+int main() {
+    Animal animal;
+    Dog dog;
+    Cat cat;
+
+    // Calling functions
+    animal.makeSound();
+    dog.makeSound();
+    cat.makeSound();
+
+    return 0;
+}
