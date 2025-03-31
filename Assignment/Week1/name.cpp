d1cb2cc4f24782e166ea18433deaa7b61abeb75c
Dhiraj Chapagain <underhade@Dhirajs-MacBook-Air.local>
2025-03-31 11:30:12 +0545

    fixed name

--- Assignment/Week1/name.cpp
+++ Assignment/Week1/name.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    string name = "Your Name"; // Replace "Your Name" with the desired name
+    string name = "Dhiraj"; // Replace "Your Name" with the desired name
     cout << "Hello, " << name << "!" << endl;
     return 0;
 }
