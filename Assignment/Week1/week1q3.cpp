26e4fc38c42f14ed4e3390dcad9157140d55e72e
Dhiraj Chapagain <underhade@Dhirajs-MacBook-Air.local>
2025-04-04 10:43:27 +0545

    q3

---
+++ Assignment/Week1/week1q3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int a, b;
+
+    cout << "Enter a number: " << endl;
+    cin >> a;
+    cout << "Enter another number: " << endl;
+    cin >> b;
+    cout << "The sum of the two numbers is: " << a + b << endl;
+    cout << "The difference of the two numbers is: " << a - b << endl;
+    cout << "The product of the two numbers is: " << a * b << endl;
+    cout << "The quotient of the two numbers is: " << a / b << endl;
+    cout << "The modulus of the two numbers is: " << a % b << endl;
+}
