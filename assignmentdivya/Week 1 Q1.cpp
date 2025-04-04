7fc081539aa2ccc297eb02a165268440555c5063
Divyaamgai <divyaamgai2@gmail.com>
2025-04-04 10:40:32 +0545

    q1

---
+++ assignmentdivya/Week 1 Q1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    cout << "Name: Divya" << endl; 
+    cout << "Age: 20" << endl;      
+    cout << "Favorite Hobby: Dancing" << endl; 
+    return 0; 
+}
