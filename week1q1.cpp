702770dbed8c1a3dd8bb1cce76e69af3a56c70b7
Asim-Silwal <silwalasim70@gmail.com>
2025-04-04 10:07:37 +0545

    Week 1 question 1

---
+++ week1q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+   
+    cout << "Name: Asim Silwal" << endl;
+    cout << "Age: 19" << endl;
+    cout << "I love riding bikes" << endl;
+
+    return 0; 
+}
