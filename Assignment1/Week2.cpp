228dd349417a14e0fa7a048705b8a48c14f81748
shohba0612 <adhikarishobha10@gmail.com>
2025-04-21 10:03:51 +0545

    another commit

--- Assignment1/Week2.cpp
+++ Assignment1/Week2.cpp
@@ -1,1 +1,15 @@
-//will write later
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 5; j >= i; j--) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
