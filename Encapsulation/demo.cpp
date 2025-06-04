509993cfa3853338ef1c23d786a24930b43d1617
Sambrid Shrestha <Xrestha9600@gmail.com>
2025-06-04 09:18:36 +0545

    changes

---
+++ Encapsulation/demo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+    void setDetails(string n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+      cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s1;
+    s1.setDetails("Alice", 20);
+    s1.display();
+
+    return 0;
+}
