fa5d082d0c82a4db014f7d8d7e59a772466826d6
kaligpaudel89 <kaligpaudel12@gmail.com>
2025-06-12 10:39:42 +0545

    q7

---
+++ Assignments/week3q7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void printData(int i) {
+    cout << "Integer: " << i << endl;
+}
+
+void printData(double f) {
+    cout << "Double: " << f << endl;
+}
+
+void printData(string s) {
+    cout << "String: " << s << endl;
+}
+
+int main() {
+    printData(25);
+    printData(3.14);
+    printData("Hello World");
+
+    return 0;
+}
