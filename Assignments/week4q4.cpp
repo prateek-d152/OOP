d348ebf3a6d579c1bd2927a03f72f566175584ad
kaligpaudel89 <kaligpaudel12@gmail.com>
2025-06-12 10:51:15 +0545

    q4

---
+++ Assignments/week4q4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void eat() {
+        cout << "Animal is eating." << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Dog is barking." << endl;
+    }
+};
+
+class GoldenRetriever : public Dog {
+public:
+    void fetch() {
+        cout << "Golden Retriever is fetching." << endl;
+    }
+};
+
+int main() {
+    GoldenRetriever gr;
+    gr.eat();
+    gr.bark();
+    gr.fetch();
+
+    return 0;
+}
