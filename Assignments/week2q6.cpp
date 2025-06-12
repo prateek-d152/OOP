94f0c8eecf5b126c067a3d3e0d43f48072182374
kaligpaudel89 <kaligpaudel12@gmail.com>
2025-06-12 10:21:50 +0545

    q6

---
+++ Assignments/week2q6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input, reversed = "";
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    for (int i = input.length() - 1; i >= 0; i--) {
+        reversed += input[i];
+    }
+
+    cout << "Reversed string: " << reversed << endl;
+
+    return 0;
+}
