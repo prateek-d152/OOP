9d40143475d151c18fc832edfbd209f022dc733a
Sayara Gautam <sayeragtm10@gmail.com>
2025-06-13 15:46:37 +0545

    q5

---
+++ OOP/Assignment.sayara/week4q5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+   
+    void getArea() {
+        cout << "Calculating area..." << endl;
+    }
+};
+
+class Rectangle : public Shape {
+private:
+    double length;
+    double width;
+
+public:
+    
+    Rectangle(double l, double w) {
+        length = l;
+        width = w;
+    }
+    void getArea() {
+        double area = length * width;
+        cout << "Area of Rectangle: " << area << endl;
+    }
+};
+
+int main() {
+  
+    Rectangle r(5.0, 3.0);
+
+    r.getArea();  
+
+    r.Shape::getArea();  
+
+    return 0;
+}
